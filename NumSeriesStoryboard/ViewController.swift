0b4fee8ce5e506d3850cf3a08880b8f3c44cfd70
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-11-19 11:49:05 +0530

    Textfield and TextView connection with viewcontroller and given name

--- NumSeriesStoryboard/ViewController.swift
+++ NumSeriesStoryboard/ViewController.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var txtNumber: UITextField!
+    
+    @IBOutlet var txtView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
