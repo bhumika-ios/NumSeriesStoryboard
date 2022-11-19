dea1f8b92aa41fd6fb57fff9dd60c7ac8ac2fb93
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-11-19 11:51:10 +0530

    write btndisplay connection then connect button

--- NumSeriesStoryboard/ViewController.swift
+++ NumSeriesStoryboard/ViewController.swift
@@ -15,7 +15,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func btnDisplay(_ sender:Any){
+        
+    }
 
 }
 
