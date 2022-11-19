7b7466b5c0a4bc0575a0683491a2c0c623d0b35a
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-11-19 11:38:34 +0530

    Initial Commit

---
+++ NumSeriesStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NumSeriesStoryboard
+//
+//  Created by Bhumika Patel on 19/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
